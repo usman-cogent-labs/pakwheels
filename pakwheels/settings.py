4341cc8bf54f5daaac21a0af3f54e5f913399fd3
usman-cogent-labs <109211422+usman-cogent-labs@users.noreply.github.com>
2023-05-30 13:17:26 +0500

    Postgres settings added

--- pakwheels/settings.py
+++ pakwheels/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pakwheels.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'paksheels',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -122,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.user'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    TYPE_CHOICES = [
+        ("ADMIN", "Admin"),
+        ("USER",  "User")
+    ]
+    type = models.CharField(max_length=5, choices=TYPE_CHOICES, default="USER")
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
